4454b0078a7cd6351c91475ea9bec66f75836013
MisterDoodlerHub <misterdoodlerhub@gmail.com>
2022-10-23 20:22:34 +0600

    Sub 231022

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 from dash.dependencies import Input, Output, State
 from dash import Dash, html, dcc
-from modules.output_table import parse_contents, process_data
+from modules.utils import get_raw_data, show_data
+from modules.soil_texture_classification import soil_texture_classification
 from dash.exceptions import PreventUpdate
 
 EXTERNAL_STYLESHEET = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -10,7 +11,17 @@ app = Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEET, suppress_callback
 
 server = app.server
 
+
+INSTRUCTIONS = """
+- This is for demonstration purpose only.
+- So far it supports excel sheet and csv file.
+- Upload an **excel sheet or csv file.**
+- Click on **'Transform Data'** Button to process your data.
+"""
+
 app.layout = html.Div(children=[
+    html.H1(children='DEMO ETL TOOL', style={'textAlign': 'center'}),
+    dcc.Markdown(INSTRUCTIONS, style={'textAlign': 'center'}),
     dcc.Upload(
         id = 'upload-data',
         children = html.Div([
@@ -26,25 +37,49 @@ app.layout = html.Div(children=[
             'textAlign': 'center',
             'margin': '10px'
         },
-        multiple=True
+        multiple=False
     ),
-
+    html.Div(id='upload-success'),
+    html.Button("Transform Data", id='btn-transform'),
+    html.H4(),
     html.Div(id='output-datatable'),
     html.Button("Download Processed Data", id='btn-download'),
     dcc.Download(id='download-dataframe-as-csv')
 ])
 
+
 @app.callback(
-    Output('output-datatable','children'),
+    Output('upload-success','children'),
+    Input('upload-data','contents'),
+    State('upload-data','filename'))
+def upload_success(raw_contents, file_name):
+    try:
+        if raw_contents is not None:
+            children = [
+            html.H5(file_name),
+            html.H5("Data upload successful", style={'color':'green'}),
+            html.Hr()]
+            return children
+    except Exception as e:
+        print(e)
+        return html.Div([html.H4('There was an error processing this file.', style={"color":"red"})])
+
+@app.callback(
+    Output('output-datatable', 'children'),
+    Input('btn-transform',"n_clicks"),
     Input('upload-data','contents'),
     State('upload-data','filename'),
-    State('upload-data','last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
-        ]
-        return children
+    prevent_initial_call=True
+)
+def transform_fn(n_clicks, raw_contents, file_name):
+    if not n_clicks:
+        PreventUpdate
+    else:
+        if raw_contents is not None:
+            df = get_raw_data(raw_contents, file_name)
+            df = soil_texture_classification(df)
+            return show_data(df)
+
 
 @app.callback(
     Output('download-dataframe-as-csv', 'data'),
@@ -53,17 +88,15 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
     State('upload-data','filename'),
     prevent_initial_call=True
 )
-def download_fn(n_clicks, raw_data, raw_file_names):
+def download_fn(n_clicks,  raw_contents, file_name):
     if not n_clicks:
         PreventUpdate
     else:
-        if raw_data is not None:
-            children = [
-                process_data(c, n) for c, n in zip(raw_data, raw_file_names)
-            ]
-            
-            output_file_name = f"{raw_file_names[0].split('.')[0]}_processed.csv"
-            return dcc.send_data_frame(children[0].to_csv, output_file_name, index=False)
+        if raw_contents is not None:
+            df = get_raw_data(raw_contents, file_name)
+            df = soil_texture_classification(df)
+            output_file_name = f"{file_name.split('.')[0]}_processed.csv"
+            return dcc.send_data_frame(df.to_csv, output_file_name, index=False)
 
 if __name__=='__main__':
     app.run_server(debug=True)
---
+++ modules/utils.py
@@ -0,0 +1,27 @@
+import base64, io
+from dash import html, dash_table
+import pandas as pd
+
+
+def get_raw_data(contents, filename):
+    _, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    if 'csv' in filename:
+        # Assume that the user uploaded a CSV file
+        df = pd.read_csv(
+            io.StringIO(decoded.decode('utf-8')))
+    elif 'xls' in filename:
+        # Assume that the user uploaded an excel file
+        df = pd.read_excel(io.BytesIO(decoded))
+    return df
+
+
+def show_data(df):
+    return html.Div([
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns],
+            page_size=15
+        ),
+        html.Hr()
+    ])
